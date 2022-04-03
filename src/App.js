9bb0aba1e9dbd778e582004e4821c254a985415e
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-03 16:17:25 +0600

    List1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Navbar } from "./component/indexcompo";
+import { List, Navbar } from "./component/indexcompo";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <List />
     </div>
   );
 };
