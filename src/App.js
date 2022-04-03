ecdb86d200efa315068e276054e7399926fb5a9d
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-03 11:52:07 +0600

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "./component/indexcompo";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
